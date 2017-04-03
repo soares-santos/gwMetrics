61ed58f36031aaee8aab518ad9d7fc52298566d5
Marcelle Soares Santos <marcelle@fnal.gov>
2017-04-03 14:07:05 -0500

    add install and setup notes

---
+++ setup.sh
@@ -0,0 +1,6 @@
+## source this file to use the environment installed at FNAL
+export CONDA_DIR=/cvmfs/des.opensciencegrid.org/fnal/anaconda2/
+export PATH=$CONDA_DIR/bin:$PATH
+source activate /data/des40.a/data/marcelle/lsstEnv
+source /data/des40.a/data/marcelle/lsstEnv/bin/eups-setups.sh
+setup lsst_sims
