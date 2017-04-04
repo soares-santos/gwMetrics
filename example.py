ba991d8a5b64a10a4368f33430f839466c9206c5
Marcelle Soares Santos <marcelle@fnal.gov>
2017-04-04 11:52:03 -0500

    updates

--- example.py
+++ example.py
@@ -17,14 +17,22 @@ import numpy as np
 import healpy as hp
 import defs as astr
 import metrics
+
+import make_maps
+
+mjds=np.array([1.,8.,15.])
+ras=np.random.rand(3)*360.
+decs=np.random.rand(3)*(-30.)
+maps=gaussian2d_from_sample_map(ras,decs,sigma_ra=5.,sigma_dec=5.,nside=128):
+
 dirdb = '/data/des40.a/data/marcelle/lsst-gw/OperationsSimulatorBenchmarkSurveys/'
 file = 'kraken_1042_sqlite.db'
 opsdb = db.OpsimDatabase(dirdb+file)
 outdir = './example-outputs/'
 resultsDb = db.ResultsDb(outDir=outdir)
-metric = metrics.AstrometryMetric()
+metric = metrics.AreaProb()
 slicer = slicers.HealpixSlicer(nside=4)
-sql0 = 'filter = "u" and night < 100'
+sql0 = 'filter = "i" and night < 50'
 cmap = plt.get_cmap('winter')
 plotDict0={'title': 'u-band. error: 0.020, z: 2.1. 1 year. opsim: kraken_1042.',  'logScale': True, 'xlabel': 'Bayes Factor','cmap': cmap,'percentileClip': None, 'colorMin':1.0, 'colorMax':100.0, 'cbarFormat':'%.1g','cbar_edge': True, 'nTicks': 10, 'aspect': 'auto','xextent': None, 'origin': None}
 HealpixSkyMap = plots.HealpixSkyMap()
--- metrics.py
+++ metrics.py
@@ -1,8 +1,34 @@
 from lsst.sims.maf.metrics import BaseMetric
-import defs as astr
+import defs 
 import numpy as np
 
 
+# Define our class, inheriting from BaseMetric
+class AreaProb(BaseMetric):
+    # Add a doc string to describe the metric.
+    """
+    Fraction of sky probability observed, in average, per event.
+    """
+    # Add our "__init__" method to instantiate the class.
+    # **kwargs allows additional values to be passed to the BaseMetric that you 
+    #     may not have been using here and don't want to bother with. 
+    def __init__(self, metricName='areaprob', **kwargs):
+        # Set the values we want to keep for our class.
+        self.MJDcol = 'expMJD'
+        self.RAcol = 'fieldRA'
+        self.DECcol = 'fieldDEC'
+        self.Fcol = 'filter'
+        cols=[self.MJDcol, self.RAcol, self.DECcol, self.Fcol]
+        # Now we have to call the BaseMetric's __init__ method, to get the "framework" part set up.
+        # We currently do this using 'super', which just calls BaseMetric's method.
+        # The call to super just basically looks like this .. you must pass the columns you need, and the kwargs.
+        super(AreaProb, self).__init__(col=cols, metricName=metricName, **kwargs)
+    # Now write out "run" method, the part that does the metric calculation.
+    def run(self, dataSlice, slicePoint=None):
+        result = defs.meanProb(dataSlice[self.MJDcol], dataSlice[self.RAcol], dataSlice[self.DECcol], dataSlice[self.Fcol], 
+                               event_prob_maps, event_mjds, 
+                               delta_t=10.,nside=16,band=None)
+        return result
 
 
 # Define our class, inheriting from BaseMetric
